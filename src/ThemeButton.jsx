9bb4f52733986985d7793a6997c7ce52a0fa5cc9
Zachary Levy <zacharyalevy@gmail.com>
2016-08-27 17:44:32 -0400

    up one parent

--- src/ThemeButton.jsx
+++ src/ThemeButton.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 
 export default class ThemeButton extends React.Component {
-  handleChangeTheme() {
-    console.log("handleChangeTheme")
-  }
   render () {
     return (
       <div>
-        <button onClick={this.handleChangeTheme}>{this.props.theme.name}</button>
+        <button onClick={this.props.handleChangeTheme.bind(this)}>{this.props.theme.name}</button>
       </div>
     )
   }
--- src/ThemeSelector.jsx
+++ src/ThemeSelector.jsx
@@ -3,13 +3,18 @@ import { render } from 'react-dom';
 import ThemeButton from './ThemeButton';
 
 export default class ThemeSelector extends React.Component {
+  handleChangeTheme() {
+    console.log("handleChangeTheme")
+    console.log(this)
+    console.log(this.props.theme)
+  }
   render () {
     return (
       <div>
         {
           this.props.themes.map(function (theme) {
             // return <ThemeButton key={theme.name} theme={theme} onClick={this.handleButtonClick} />
-            return <ThemeButton key={theme.name} theme={theme} />
+            return <ThemeButton key={theme.name} theme={theme} handleChangeTheme={this.handleChangeTheme}/>
           }.bind(this))
         }
       </div>
