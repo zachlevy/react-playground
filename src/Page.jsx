197a94f7f614555ff979fbf05448d6a348d74126
Zachary Levy <zacharyalevy@gmail.com>
2016-08-28 14:19:57 -0400

    sidebar left or right

--- src/Page.jsx
+++ src/Page.jsx
@@ -6,29 +6,45 @@ import Subpage from "./Subpage"
 
 export default class Page extends React.Component {
   render () {
+    // if subpages
     if (Array.isArray(this.props.content.body)) {
-      return (
-        <div>
-          <div>
+      // set up main and sidebar
+      let main = (
+        <div className="col-xs-12 col-sm-8">
+          {this.props.content.body.map(function(subpage, index) {
+             return <Subpage key={subpage.slug} content={subpage} />
+          })}
+        </div>
+      )
+      let sidebar = (
+        <div className="col-xs-12 col-sm-4">
+          <ul>
             {this.props.content.body.map(function(subpage, index) {
-               return <Subpage key={subpage.slug} content={subpage} />
+               return <li key={subpage.slug}>{subpage.title}</li>
             })}
-          </div>
-          <div>
-            <ul>
-              {this.props.content.body.map(function(subpage, index) {
-                 return <li key={subpage.slug}>{subpage.title}</li>
-              })}
-            </ul>
-          </div>
+          </ul>
+        </div>
+      )
+      // put columns into an array
+      let columns;
+      if (this.props.sidebar == "left") {
+        columns = [sidebar, main]
+      } else {
+        columns = [main, sidebar]
+      }
+
+      return (
+        <div className="row">
+          {columns.map(function(column, index) {
+             return <div key={index}>{column}</div>
+          })}
         </div>
       )
     } else {
-      let content = this.props.content.body
       return (
         <div>
-          <h1>{content.title}</h1>
-          <p>{content.body}</p>
+          <h1>{this.props.content.title}</h1>
+          <p>{this.props.content.body}</p>
         </div>
       )
     }
